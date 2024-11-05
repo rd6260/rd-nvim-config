9a1758bd0236ce60ddf8e2c7a8dc963032cd2381
rd_626 <rohandas.zero@gmail.com>
2024-11-05 12:13:25 +0530

    adds go snippets

--- init.lua
+++ init.lua
@@ -4,6 +4,9 @@ vim.g.mapleader = " "
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
+
+vim.g.lua_snippets_path = vim.fn.stdpath "config" .. "/lua/snippets/"
+
 if not vim.loop.fs_stat(lazypath) then
   local repo = "https://github.com/folke/lazy.nvim.git"
   vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
