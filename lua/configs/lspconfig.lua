1780a86caef14a5a46979b682893aa39721da922
rd_626 <rohandas.zero@gmail.com>
2024-11-07 13:48:33 +0530

    changes snippets

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,8 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "gopls", "pyright" }
+local util = require "lspconfig/util"
+local servers = { "html", "cssls", "pyright", "gopls" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -14,6 +15,33 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
+
+
+-- lspconfig.gopls.setup {
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+--   cmd = {"gopls"},
+--   filetypes = { "go", "gomod", "gowork", "gotmpl" },
+--   root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+--   settings = {
+--     gopls = {
+--       completeUnimported = true,
+--       usePlaceholders = true,
+--       analyses = {
+--         unusedparams = true
+--       }
+--     }
+--   }
+-- }
+
+-- auto format when save
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.go",
+    callback = function()
+        vim.lsp.buf.format({ async = false })
+    end,
+})
+
 
 -- typescript
 lspconfig.ts_ls.setup {
--- lua/snippets/go.lua
+++ lua/snippets/go.lua
@@ -3,12 +3,38 @@
 local ls = require "luasnip"
 local s = ls.snippet
 local t = ls.text_node
-local i = ls.inset_node
+local i = ls.insert_node
 
 ls.add_snippets("go", {
   s("main", {
-    t('func main() {'),
-    t('}')
+    t({
+      "package main", "",
+      "import (",
+      '\t"fmt"',
+      ")","",
+      "func main() {",
+      "\t"
+    }),
+    i(1, 'fmt.Println("Hello; World!")'),
+    t({ "", "}" })
+  }),
+
+  s("fpl", {
+    t("fmt.Println("), i(1), t(")")
+  }),
+
+  s("cerr", {
+    t({
+      "if err != nil {",
+      "\tpanic(err)",
+      "}"
+    })
   })
+
+
+  -- s("test", {
+  --   t("you know"),
+  --   t({"this", "is", "a", "test"})
+  -- })
 })
 
